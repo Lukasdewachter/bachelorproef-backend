6b8e5a228dbc4724a285760cca1c3b93df980b8b
aminkhatibi <aminkhatibi@icloud.com>
2022-02-22 13:01:47 +0100

    Student aangepast op basis van database

--- src/main/java/com/authenticatie/Student.java
+++ src/main/java/com/authenticatie/Student.java
@@ -9,23 +9,52 @@ import javax.persistence.Id;
 public class Student {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
-
+    private Integer idstudent;
     private String name;
+    private String surname;
+    private String mail;
+    private Integer tel;
+    private String adress;
+    private String fieldofstudy;
+    private boolean coordinator;
 
-    public Integer getId() {
-        return id;
+    public Integer getId() {return idstudent;}
+    public void setId(Integer idstudent) {
+        this.idstudent = idstudent;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
+    public String getName() {return name;}
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getmail() {
+        return mail;
+    }
+    public void setMail(String mail) {this.mail = mail;}
+
+    public Integer getTel() {return tel;}
+    public void setTel(Integer tel) {
+        this.tel = tel;
+    }
+
+    public String getAdress() {return adress;}
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getFieldOfStudy() {return fieldofstudy;}
+    public void setFieldOfStudy(String fieldofstudy) {this.fieldofstudy = fieldofstudy;}
+
+    public boolean getCoordinator() {
+        return coordinator;
+    }
+    public void setCoordinator(boolean coordinator) {this.coordinator = coordinator;}
 }
