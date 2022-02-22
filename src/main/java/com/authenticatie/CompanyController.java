4b1db34b6c2800184df2c831dae1245f51d3b0b7
aminkhatibi <aminkhatibi@icloud.com>
2022-02-22 13:47:07 +0100

    Company en aanpassing Student

---
+++ src/main/java/com/authenticatie/CompanyController.java
@@ -0,0 +1,39 @@
+package com.authenticatie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@RequestMapping(path="/company") // This means URL's start with professor (after Application path)
+@CrossOrigin(origins = "http://localhost:3000")
+
+public class CompanyController {
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewCompany (@RequestParam String companyname) {
+        Company c = new Company();
+        c.setName(companyname);
+        companyRepository.save(c);
+        return "Saved";
+    }
+
+    //TODO - Delete
+    @PostMapping(path="/delete")
+    public @ResponseBody String deleteCompany (@RequestParam int id) {
+        companyRepository.deleteById(id);
+        return "Deleted";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Company> getAllCompanies() {
+        // This returns a JSON or XML with the users
+        return companyRepository.findAll();
+    }
+}
