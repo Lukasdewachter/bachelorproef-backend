92f99810e9e10c034f298c1735c1be50625ab0e7
Wannes Van de Putte <vandeputte.wannes@gmail.com>
2022-02-22 10:54:08 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bachelorproef - Backend
+
+Gevolgde tutorial: [Getting Started - Spring IO](https://spring.io/guides/gs/accessing-data-mysql/)
+
+
+## Services
+1. Authenticatie
+2. User management
+3. Masterproeven en voorkeuren
+4. Communicatie met de gebruikers
+
+## MySQL - *JPA*
+Table: `demo_projectweek`  
+User: `DemoProjectweek`  
+WW: `demo`
---
+++ src/main/java/com/authenticatie/StudentController.java
@@ -0,0 +1,39 @@
+package com.authenticatie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@RequestMapping(path="/student") // This means URL's start with /student (after Application path)
+@CrossOrigin(origins = "http://localhost:3000")
+
+public class StudentController {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewStudent (@RequestParam String name) {
+        Student n = new Student();
+        n.setName(name);
+        studentRepository.save(n);
+        return "Saved";
+    }
+
+    //TODO - Delete
+    @PostMapping(path="/delete")
+    public @ResponseBody String deleteStudent (@RequestParam int id) {
+        studentRepository.deleteById(id);
+        return "Deleted";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Student> getAllStudents() {
+        // This returns a JSON or XML with the users
+        return studentRepository.findAll();
+    }
+}
